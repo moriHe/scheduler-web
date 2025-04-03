1c52b86350813d1319d3d2829391d98e5946663c
Moritz Hettich <mor.hettich@gmail.com>
2025-04-03 22:16:58 +0200

    add back button blockedtime

--- src/options/optionsListeners.js
+++ src/options/optionsListeners.js
@@ -2,7 +2,7 @@ import { resetUsers, uploadUsers } from "./options.js"
 
 // Back to start page
 document.getElementById("back-button").addEventListener("click", () => {
-  window.location.href = "../index.html"; // Navigate back to index.html
+  window.location.href = "/index.html"; // Navigate back to index.html
 });
 
 // Reset users in localStorage
